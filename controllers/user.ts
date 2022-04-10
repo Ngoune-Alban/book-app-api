cc50e6e29d77a26a4c6a9fd01fca85612ce11806
Ngoune Alban <alban.tyrex@gmail.com>
2022-04-10 16:07:46 +0100

    feat: encrypt user's password before storing it in the database

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-// const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 // const jwt = require('jsonwebtoken');
 import { Request, Response } from 'express';
 
@@ -9,14 +9,21 @@ exports.login = (req: Request, res: Response) => {
 };
 
 
-exports.register = (req: Request, res: Response) => {
-    console.log(req.body);
-    const user = new User({
-        email: req.body.email,
-        password: req.body.password
-    });
-    user.save()
-        .then(() => {
-            res.status(201).json({ message: "Successfully created user !" })
-        })
+exports.register = async (req: Request, res: Response) => {
+    try {
+        const password = await bcrypt.hash(req.body.password, 2);
+        const user = new User({
+            email: req.body.email,
+            password
+        });
+        await user.save();
+        res.status(201).json("Successfully created user !")
+    }
+    catch (e) {
+        const err = e as Error;
+        if (err.name == "ValidationError") {
+            res.status(400).json(err.message)
+        }
+        else res.status(500).json("An unexpeteced error occur !");
+    }
 };
