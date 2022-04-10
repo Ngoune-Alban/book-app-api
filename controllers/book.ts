afbd45898d34470695bc9ec1a52c217eb1f6e2cb
Ngoune Alban <alban.tyrex@gmail.com>
2022-04-10 17:58:41 +0100

    feat: adding middleware on while adding a book

--- controllers/book.ts
+++ controllers/book.ts
@@ -1,13 +1,25 @@
 import { Request, Response } from 'express';
 const Book = require('../models/book');
 
-
-exports.createOneBook = async (req: Request, res: Response) => {
-    const book = new Book({ ...req.body });
-    console.log(book);
-    await book.save();
-    return res.status(201).json("Successfully created book !")
+interface RequestCustom extends Request {
+    userId: string;
 }
+
+exports.createOneBook = async (req: RequestCustom, res: Response) => {
+    try {
+        const book = new Book({ ...req.body, authorId: req.userId });
+        await book.save();
+        return res.status(201).json("Successfully created book !")
+    }
+    catch (e) {
+        const err = e as Error;
+        if (err.name == "ValidationError") {
+            res.status(400).json(err.message)
+        }
+        else res.status(500).json("An unexpeteced error occur !");
+    }
+}
+
 // exports.findAllBooks = (req: Request, res: Response) => {
 // }
 // exports.findOneBook = (req: Request, res: Response) => {
--- routes/book.ts
+++ routes/book.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-// const auth = require('../middlewares/auth');
+const auth = require('../middlewares/auth');
 const bookCtrl = require('../controllers/book');
 const router = express.Router();
 
-router.post('/', bookCtrl.createOneBook);
+router.post('/', auth, bookCtrl.createOneBook);
 // router.get('/', auth, bookCtrl.findAllBooks);
 // router.get('/:id', auth, bookCtrl.findOneBook);
 // router.put('/:id', auth, bookCtrl.updateOneBook);
